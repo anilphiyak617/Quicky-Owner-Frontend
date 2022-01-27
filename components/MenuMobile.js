6c8713481c23fae46317ca347ea51be9bbeed283
AkaCoder007 <78375228+AkaCoder007@users.noreply.github.com>
2022-01-27 21:47:01 +0530

    =made changes to make header component responsive for mobile

---
+++ components/MenuMobile.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Transition } from "@headlessui/react";
+const Menumobile = ({ menuOpen }) => {
+  const navList = [
+    { title: "Home" },
+    { title: "Menu" },
+    { title: "Services" },
+    { title: "Create Account" },
+  ];
+  return (
+    <Transition
+      show={menuOpen}
+      enter="transition ease-in duration-200 transform"
+      enterFrom="opacity-0 scale-100"
+      enterTo="opacity-100 scale-100"
+      leave="transition ease-in duration-75 transform"
+      leaveFrom="opacity-100 scale-100"
+      leaveTo="opacity-0 scale-95"
+    >
+      <div className="md:hidden" id="mobile-menu">
+        <div className="grid grid-cols-1 px-2 pt-2 pb-3 space-y-1 sm:px-3">
+          {navList.map((item) => (
+            <a
+              href={item.link}
+              key={item.link}
+              className="text-gray-900 text-left font-normal hover:text-green-500 px-3 py-2  text-md border-b-2 select-none "
+            >
+              {item.title}
+            </a>
+          ))}
+        </div>
+      </div>
+    </Transition>
+  );
+};
+
+export default Menumobile;
