cbcee8ed068276aecd1417103819cbb92b5d6f0b
AkaCoder007 <78375228+AkaCoder007@users.noreply.github.com>
2022-01-27 21:43:40 +0530

    =modified git ignore file

--- components/Header.js
+++ components/Header.js
@@ -4,10 +4,14 @@ import img from "../public/images/logo.jpg";
 import { useState } from "react";
 import Menu from "./Menu";
 import { Transition } from "@headlessui/react";
+import xIcon from "../public/x.svg";
+import Test from "./Test";
+import Menumobile from "./MenuMobile";
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  let height = `${menuOpen ? "shadow-md h-[100vh]" : "shadow-md"}`;
   return (
-    <div className="shadow-md h-[100vh]">
+    <div className={height}>
       {/* // menu bar */}
       <div className=" flex items-center space-x-4 p-4 h-20 lg:h-24 lg:px-10 ">
         {!menuOpen && (
@@ -27,14 +31,13 @@ const Header = () => {
         {menuOpen && (
           <>
             <div
-              className="transition-all ease-in-out duration-1000"
+              className="relative transition-all h-6 w-6 ease-in-out duration-1000"
               onClick={() => setMenuOpen(false)}
             >
-              ❌
+              <Image src={xIcon} alt="xicon" layout="fill" />
             </div>
           </>
         )}
-
         {/* logo */}
         <div className="cursor-pointer relative h-10 w-16  lg:h-16 lg:w-24 ">
           <Image src={img} alt="logo" layout="fill" />
@@ -53,52 +56,8 @@ const Header = () => {
           </button>
         </div>
       </div>
-      <Transition
-        show={menuOpen}
-        enter="transition ease-out duration-100 transform"
-        enterFrom="opacity-0 scale-95"
-        enterTo="opacity-100 scale-100"
-        leave="transition ease-in duration-75 transform"
-        leaveFrom="opacity-100 scale-100"
-        leaveTo="opacity-0 scale-95"
-      >
-        {(ref) => (
-          <div className="md:hidden" id="mobile-menu">
-            <div
-              ref={ref}
-              className="grid grid-cols-1 px-2 pt-2 pb-3 space-y-1 sm:px-3"
-            >
-              <a
-                href="#"
-                className="text-gray-900 font-medium hover:text-green-500 px-3 py-2  text-md border-b-2 "
-              >
-                Home
-              </a>
-
-              <a
-                href="#"
-                className="text-gray-900 font-medium hover:text-green-500  px-3 py-2  text-md border-b-2"
-              >
-                Menu
-              </a>
-
-              <a
-                href="#"
-                className="text-gray-900  font-medium hover:text-green-500  px-3 py-2 text-md border-b-2"
-              >
-                Services
-              </a>
-
-              <a
-                href="#"
-                className="text-gray-900 font-medium hover:text-green-500  py-2  text-md border-b-2"
-              >
-                How it works
-              </a>
-            </div>
-          </div>
-        )}
-      </Transition>
+      {/* {menu mobile} */}
+      <Menumobile menuOpen={menuOpen} />
     </div>
   );
 };
